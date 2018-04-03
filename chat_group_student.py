bfcb5f897e75ff5b10fad1668dfcae0f33fd544a
1511489992 <1511489992@qq.com>
2018-04-03 22:59:42 +0800

    NYUClasses first submission

--- chat_group_student.py
+++ chat_group_student.py
@@ -29,45 +29,92 @@ class Group:
         
     #implement        
     def is_member(self, name):
-        return False
+        if name in self.members:
+            return True
+        else:
+            return False
             
     #implement
     def leave(self, name):
+        self.disconnect(name)
+        del self.members[name]
         return
         
     #implement                
     def find_group(self, name):
         found = False
         group_key = 0
+        for k,v in self.chat_grps.items():
+            if name in v:
+                found = True
+                group_key = k
+                break
         return found, group_key
         
     #implement                
     def connect(self, me, peer):
+        self.members[me] = S_TALKING
+        self.members[peer] = S_TALKING
         #if peer is in a group, join it
         peer_in_group, group_key = self.find_group(peer)
         # otherwise, create a new group with you and your peer
+        if peer_in_group:
+            self.chat_grps[group_key].append(me)
+        else:
+            self.grp_ever += 1
+            self.chat_grps[self.grp_ever] = [me] + [peer]
         return
 
     #implement                
     def disconnect(self, me):
+        self.members[me] = S_ALONE
         # find myself in the group, quit
+        for grp_key,group in self.chat_grps.items():
+            if me in group:
+                group.remove(me)
+                break
+        if len(group) == 1:
+            self.members[group[0]] = S_ALONE
+            self.grp_ever -= 1
+            del self.chat_grps[grp_key]
         return
         
     def list_all(self):
         # a simple minded implementation
+#        full_list = "Users: ------------" + "\n"
+#        full_list += str(self.members) + "\n"
+#        full_list += "Groups: -----------" + "\n"
+#        full_list += str(self.chat_grps) + "\n"
+#        return full_list
         full_list = "Users: ------------" + "\n"
-        full_list += str(self.members) + "\n"
+        for k,v in self.members.items():
+            full_list += k + ' - '
+            if v == S_ALONE:
+                full_list += 'ALONE\n'
+            else:
+                full_list += 'TALKING\n'
         full_list += "Groups: -----------" + "\n"
-        full_list += str(self.chat_grps) + "\n"
+        full_list += "Group Number\tMembers\n"
+        for k,v in self.chat_grps.items():
+            full_list += '     {:<10}'.format(k) + str(v) + '\n'
+        full_list += '\n'
         return full_list
 
     #implement
     def list_me(self, me):
         # return a list, "me" followed by other peers in my group
-        my_list = []
+        if self.members[me] == S_ALONE:
+            return ['Error']
+        my_list = [me]
+        for k,v in self.chat_grps.items():
+            if me in v:     
+                for peer in v:
+                    if peer != me:
+                        my_list.append(peer)
+                break
         return my_list
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
 	g = Group()
 	g.join('a')
 	g.join('b')
@@ -75,3 +122,5 @@ if __name__ == "__main__":
 
 	g.connect('a', 'b')
 	print(g.list_all())
+  
+ 
